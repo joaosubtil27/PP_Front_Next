b725d2f6308e3bac10ca8f7bb93e8db617a50c3b
Joao <joaoarthursubtil@gmail.com>
2025-07-15 18:11:36 -0300

    Iniciando Sidebar

---
+++ ctgram/src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export function Sidebar(){
+    return (
+        <div className="w-40 border border-dashed border-2 border-amber-950">
+<h1> ola </h1>
+<h2>ola ola</h2>
+        </div>
+    )
+}
