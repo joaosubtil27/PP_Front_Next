57d974858f03dddf985758419c08e6302062b82d
Joao <joaoarthursubtil@gmail.com>
2025-07-15 21:30:44 -0300

    Not_found

--- ctgram/src/app/not-found.tsx
+++ ctgram/src/app/not-found.tsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 export default function NotFound() {
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-100">
-            <main className=" p-10 bg-gray-300 border-md">
-            <h1 className="">ERRO 404</h1>
-            <h2>Vish! Essa página não existe. Volte à página inicial</h2>
+            <main className="flex flex-col items-center justify-center p-10 gap-5 bg-gray-300 rounded-md">
+            <h1 className="text-4xl"><strong>ERROR 404</strong></h1>
+            <h2>Vish! Essa página não existe. Volte à página inicial.</h2>
             <Link href="/">
                 <Button>Início</Button>
             </Link>
