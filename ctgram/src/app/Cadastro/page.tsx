af99f6eac378d81051570852b0ca86865c0e451f
Joao <joaoarthursubtil@gmail.com>
2025-07-16 18:02:07 -0300

    Socorro Deus

--- ctgram/src/app/Cadastro/page.tsx
+++ ctgram/src/app/Cadastro/page.tsx
@@ -10,23 +10,24 @@ import { z, ZodError } from "zod"
 import Image from "next/image"
 import type React from "react"
 import Link from "next/link"
+import { AxiosError } from "axios"
 
 const signUpSchema = z.object({
-    nome: z.string().trim().min(1, { message: "Informe o nome" }),
+    username: z.string().trim().min(1, { message: "Informe o nome" }),
     email: z.string().email({ message: "E-mail inválido" }),
     password: z.string().min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
-    npassword: z.string({message: "Confirme a senha"})
-}).refine ((data) => data.password === data.npassword,{
-message: "As senhas não são iguais",
-path: ["npassword"],
+    newpassword: z.string({ message: "Confirme a senha" })
+}).refine((data) => data.password === data.newpassword, {
+    message: "As senhas não são iguais",
+    path: ["newpassword"],
 })
 export default function Cadastro() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-    const [nome, setNome] = useState("");
-    const [npassword, setNewPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [newpassword, setNewPassword] = useState("");
     const [isFormValid, setForm] = useState(false);
 
     const path = usePathname();
@@ -34,15 +35,15 @@ export default function Cadastro() {
     const page = query.get("page");
     const router = useRouter();
 
-  useEffect(() => {
-    try {
+    useEffect(() => {
+        try {
 
-      signUpSchema.parse({ nome, email, password, npassword });
-      setForm(true); 
-    } catch (error) {
-      setForm(false); 
-    }
-  }, [nome, email, password, npassword]);
+            signUpSchema.parse({ username, email, password, newpassword });
+            setForm(true);
+        } catch (error) {
+            setForm(false);
+        }
+    }, [username, email, password, newpassword]);
 
     async function onSubmit(e: React.FormEvent) {
         e.preventDefault()
@@ -50,24 +51,31 @@ export default function Cadastro() {
             setIsLoading(true)
 
             const data = signUpSchema.parse({
-                nome,
+                username,
                 email,
                 password,
-                npassword,
+                newpassword
             })
-            // await api.post("/users", data);
+            console.log(username, email, password, newpassword);
+            const envio = {
+                email: data.email,
+                password: data.password,
+                username: data.username,
+            }
+            await api.post("/user", envio);
 
-            // if(confirm("Cadastro realizado!. Ir para a página inicial?")){
-            //     <Link href="/"/>
-            // }
+            if (confirm("Cadastro realizado!")){}
 
         } catch (error) {
             if (error instanceof ZodError) {
                 return alert(error.issues[0].message)
             }
+            if (error instanceof AxiosError)
+                return alert(error.response?.data.message)
+
             alert("Não foi possível cadastrar!");
         }
-        finally{
+        finally {
             setIsLoading(false)
         }
     }
@@ -76,7 +84,7 @@ export default function Cadastro() {
             <main className="bg-gray-300 w-max p-10 rounded-md flex items-center flex-col md:min-w-[462px]">
                 <Image src="https://www.ctjunior.com.br/images/logo/logo-branca-reta-noSlogan.svg" width={300} height={120.75} alt="" className="pb-3" />
                 <form onSubmit={onSubmit} className="w-full flex flex-col gap-5 items-center" >
-                    <Input required legend="Nome" type="text" placeholder="Nome" onChange={(e) => setNome(e.target.value)} />
+                    <Input required legend="Nome" type="text" placeholder="Nome" onChange={(e) => setUsername(e.target.value)} />
                     <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)} />
                     <Input required legend="Senha" type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
                     <Input required legend="Digite a senha novamente" type="password" placeholder="Confirmação de Senha" onChange={(e) => setNewPassword(e.target.value)} />
